b4b39bc388093aa2083d8fb7357b374b4321bf3b
Service User <service-user@inkomerc.lv>
2020-10-05 14:24:53 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if(args.length > 0) {
+            File file = new File(args[0]);
+
+           
+        }
+    }
+}
