ed3d50684cbc1fcb66c2813bd1298d017ec538e3
sergej <tulitas1@gmail.com>
2020-10-06 20:05:27 +0300

    It`s work, need to add sort by times count.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,66 +1,122 @@
 package com.company;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static java.lang.System.out;
-import static java.nio.file.Files.lines;
-import static java.util.Comparator.comparingLong;
-import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Stream.of;
+
 
 public class Main {
+    private HashMap<String, Integer> map = new HashMap<String, Integer>();
 
     public static void main(String[] args) throws IOException {
-        for (String s: args) {
-            System.out.println(s);
-//        out.println("hello");
-//        lines(Path.of(args[0]))
-//                .flatMap(l -> of(l.split("[^\\p{IsAlphabetic}]")))
-//                .filter(l -> !l.isEmpty())
-//                .map(String::toLowerCase)
-//                .collect(groupingBy(identity(), counting()))
-//                .entrySet().stream()
-//                .sorted(comparingLong(Map.Entry<String, Long>::getValue).reversed())
-//                .forEach(out::println);
+        Main project = new Main();
+        if (args.length > 0) {
+            File file = new File(args[0]);
+            Scanner scanner = new Scanner(file);
+            project.analyse(scanner);
 
-//        try (BufferedReader br = new BufferedReader(new FileReader("book/book.txt"))) {
-//            StringBuilder sb = new StringBuilder();
-//            String line = br.readLine();
-//
-//            while (line != null) {
-//                sb.append(line);
-//                sb.append(System.lineSeparator());
-//                line = br.readLine();
-//
-//
-//            }
-//            String everything = sb.toString();
-//            List<String> words = new ArrayList<>();
-//
-//            words.add(everything);
+            project.showResults();
+        }
 
 
-//            int k[] = new int[]{-1, 0};
-//            for (int i = 0; i < words.size(); i++) {
-//                if (!words.get(k[1]).equals(words.get(i)) || k[0] < 0) {
-//                    int c = 1;
-//                    for (int p = i + 1; p < words.size(); p++) {
-//                        if (words.get(i).equals(words.get(p))) {
-//                            c++;
-//                        }
-//                    }
-//                    if (k[0] < c) {
-//                        k[0] = c;
-//                        k[1] = i;
-//                    }
-//                }
-//            }
-//            System.out.println("Самое повторяющееся слово: " + words.get(k[1]) + " (встречается " + k[0] + " раз)");
+//        Scanner INPUT_TEXT = project.readFile();
+
+
+
+    }
+
+    /**
+     * logic to count the occurences of words matched by REGEX in a scanner that
+     * loaded some text
+     *
+     * @param scanner the scanner holding the text
+     */
+    private void analyse(Scanner scanner) {
+
+        String pattern = "[a-zA-Z'-]+";
+        Pattern r = Pattern.compile(pattern);
+
+        while (scanner.hasNext()) {
+            // read next word
+            String Stringcandidate = scanner.next();
+
+            // see if pattern matches (boolean find)
+            Matcher matcher = r.matcher(Stringcandidate);
+            if (matcher.find()) {
+                String matchedWord = matcher.group();
+                //System.out.println(matchedWord); //check what is matched
+                this.addWord(matchedWord);
+
+            }
 
         }
+        scanner.close();// Close your Scanner.
     }
+
+    /**
+     * adds a word to the <word,count> Map if the word is new, a new entry is
+     * created, otherwise the count of this word is incremented
+     */
+    private void addWord(String matchedWord) {
+
+        if (map.containsKey(matchedWord)) {
+            // increment occurrence
+            int occurrence = map.get(matchedWord);
+            occurrence++;
+            map.put(matchedWord, occurrence);
+        } else {
+            // add word and set occurrence to 1
+            map.put(matchedWord, 1);
+        }
+
+    }
+
+    /**
+     * reads a file from disk and returns a scanner to analyse it
+     *
+     * @return the file from disk as scanner
+     */
+    public Scanner readFile() {
+
+        Scanner scanner = null;
+
+        /* use that for reading a file from disk
+         * try { scanner = new Scanner(new
+         * File("moviereview.txt")).useDelimiter(" "); } catch (Exception e) {
+         * e.printStackTrace(); }
+         */
+
+        scanner = new Scanner("auto bush trumped her tomato in the petunia auto");
+
+        return scanner;
+    }
+
+    /**
+     * prints the matched words and their occurrences
+     * in a readable way
+     */
+    private void showResults() {
+
+        for (HashMap.Entry<String, Integer> matchedWord : map.entrySet()) {
+            int occurrence = matchedWord.getValue();
+            System.out.print("\"" + matchedWord.getKey() + "\" appears " + occurrence);
+            if (occurrence > 1) {
+                System.out.print(" times\n");
+            } else {
+                System.out.print(" time\n");
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
 
